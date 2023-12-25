55943dd329273f0a314226d26ebe9fa10347004c
Muhammad Saad <github.5s32y@slmail.me>
2023-12-25 16:50:38 +0000

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+// /src/app.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const db = require('./db');
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
